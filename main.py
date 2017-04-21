3519bd87fcf397f3598b88c51a9968cdb32f93c2
Alex Malyutin <malutin.alex@gmail.com>
2017-04-22 02:02:16 +0300

    Fixxxxxxxxxxx

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from celery import Celery
 import default_config
 
-celery = Celery('main', backend=default_config.CELERY_RESULT_BACKEND, broker=default_config.CELERY_BROCKER_URL)
+celery = Celery('main', backend=default_config.CELERY_RESULT_BACKEND, broker=default_config.CELERY_BROKER_URL)
 
 import tasks
